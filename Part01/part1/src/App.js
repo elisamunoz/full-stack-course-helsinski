8218681aba7664b9678d7acf866d8b2391cc6e17
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-08-18 17:02:10 +0200

    test

--- Part01/part1/src/App.js
+++ Part01/part1/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const Hello = props => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  );
+};
 
 function App() {
+  const name = "Eli";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Greetings</h1>
+        <Hello name={name} />
+        <Hello name="Laitor" />
       </header>
     </div>
   );
