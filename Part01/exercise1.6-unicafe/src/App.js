858ae0e5a28d2b71925b32ae69d38af879dab9d4
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-08-20 00:13:40 +0200

    exercise 1.7

--- Part01/exercise1.6-unicafe/src/App.js
+++ Part01/exercise1.6-unicafe/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  const allFeedback = good + neutral + bad;
+  const average = allFeedback === 0 ? 0 : (good - bad) / allFeedback;
+  const positivePercentage = allFeedback === 0 ? 0 : (good / allFeedback) * 100;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Give feedback</h1>
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+
+      <h2>Statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {allFeedback}</p>
+      <p>average: {average}</p>
+      <p>positive: {positivePercentage} %</p>
     </div>
   );
-}
+};
 
 export default App;
