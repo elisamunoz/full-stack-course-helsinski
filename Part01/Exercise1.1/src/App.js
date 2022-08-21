cb38876279e381664e27fcc68eed1a486e4bc2bf
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-08-21 22:56:14 +0200

    exercise 2.3

--- Part01/Exercise1.1/src/App.js
+++ Part01/Exercise1.1/src/App.js
@@ -1,53 +1,52 @@
-const Header = props => <h1>{props.course}</h1>;
+const Course = ({ children }) => <div>{children}</div>;
 
-const Content = props => {
-  const Part = partProps => (
-    <p>
-      {partProps.part} {partProps.exercise}
-    </p>
-  );
+const Header = ({ course }) => <h1>{course}</h1>;
 
-  return (
-    <>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </>
-  );
-};
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
 
-const Total = props => <p>Number of exercises {props.exercises}</p>;
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+    <Part part={parts[3]} />
+  </>
+);
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7
-      },
-      {
-        name: "State of a component",
-        exercises: 14
-      }
-    ]
-  };
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    },
+    {
+      name: "Redux",
+      exercises: 11
+    }
+  ];
 
-  const totalExercises =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
-
+  parts.reduce((acc, val) => {}, 0);
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={totalExercises} />
-    </div>
+    <Course>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total sum={parts.reduce((acc, val) => (acc += val.exercises), 0)} />
+    </Course>
   );
 };
 
