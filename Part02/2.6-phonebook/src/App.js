7c40c3162e6f2316ea171cab7f386314826e2f1f
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-08-23 13:14:37 +0200

    2.11

--- Part02/2.6-phonebook/src/App.js
+++ Part02/2.6-phonebook/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 const Filter = ({ value, onChange }) => (
   <form>
     <div>
@@ -36,15 +36,17 @@ const InputForm = ({ value, onChange, text }) => (
 const Persons = ({ myKey, children }) => <li key={myKey}>{children}</li>;
 
 const App = () => {
-  const [persons, setPerson] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 }
-  ]);
+  const [persons, setPerson] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then(response => {
+      setPerson(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   const addPerson = event => {
     event.preventDefault();
